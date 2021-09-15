e1ba6a8e62797011c6ca9c447e081e7ff01bb61a
zed <90huanw@gmail.com>
2021-09-15 22:57:04 +0800

    modules

--- cmd/exce.go
+++ cmd/exce.go
@@ -11,8 +11,10 @@ var execCmd = &cobra.Command{
 	Short: "调用和执行 modules",
 
 	Run: func(cmd *cobra.Command, args []string) {
+		mode, _ := cmd.Flags().GetString("mode")
 		hosts, _ := cmd.Flags().GetString("hosts")
 		modules, _ := cmd.Flags().GetString("modules")
+		log.Println(mode)
 		log.Println(hosts)
 		log.Println(modules)
 		log.Println(args)
@@ -20,7 +22,8 @@ var execCmd = &cobra.Command{
 }
 
 func init() {
+	execCmd.Flags().StringP("mode", "M", "ssh", "连接模式(ssh|server)")
 	execCmd.Flags().StringP("hosts", "H", "all", "主机")
-	execCmd.Flags().StringP("modules", "m", "shell", "模块名")
+	execCmd.Flags().StringP("modules", "m", "shell", "模块")
 	RootCmd.AddCommand(execCmd)
 }
